539cda00d7b40c05ea54e77de29f8e7627b249f7
ddo11 <chloedo0410@yahoo.com>
2019-06-26 15:42:29 -0400

    complete initial UI for Lab8

--- COMP123-S2019-LESSON8B/LAB8Form.Designer.cs
+++ COMP123-S2019-LESSON8B/LAB8Form.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             this.TitleLabel = new System.Windows.Forms.Label();
+            this.InfoGroupBox = new System.Windows.Forms.GroupBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.AgeTextBox = new System.Windows.Forms.TextBox();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.InfoGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // TitleLabel
@@ -41,16 +48,80 @@
             this.TitleLabel.TabIndex = 0;
             this.TitleLabel.Text = "Lab08";
             // 
-            // LAB8
+            // InfoGroupBox
+            // 
+            this.InfoGroupBox.Controls.Add(this.AgeTextBox);
+            this.InfoGroupBox.Controls.Add(this.AgeLabel);
+            this.InfoGroupBox.Controls.Add(this.NameTextBox);
+            this.InfoGroupBox.Controls.Add(this.NameLabel);
+            this.InfoGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InfoGroupBox.Location = new System.Drawing.Point(13, 72);
+            this.InfoGroupBox.Name = "InfoGroupBox";
+            this.InfoGroupBox.Size = new System.Drawing.Size(590, 368);
+            this.InfoGroupBox.TabIndex = 1;
+            this.InfoGroupBox.TabStop = false;
+            this.InfoGroupBox.Tag = "";
+            this.InfoGroupBox.Text = "Info";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(12, 38);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(403, 73);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Enter Name:";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(421, 68);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(163, 38);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // AgeTextBox
+            // 
+            this.AgeTextBox.Location = new System.Drawing.Point(418, 178);
+            this.AgeTextBox.Name = "AgeTextBox";
+            this.AgeTextBox.Size = new System.Drawing.Size(163, 38);
+            this.AgeTextBox.TabIndex = 3;
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AgeLabel.Location = new System.Drawing.Point(66, 148);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(346, 73);
+            this.AgeLabel.TabIndex = 2;
+            this.AgeLabel.Text = "Enter Age:";
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.SubmitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitButton.Location = new System.Drawing.Point(518, 690);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 2;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = false;
+            // 
+            // LAB8Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(614, 769);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.InfoGroupBox);
             this.Controls.Add(this.TitleLabel);
-            this.Name = "LAB8";
+            this.Name = "LAB8Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LAB8";
+            this.InfoGroupBox.ResumeLayout(false);
+            this.InfoGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,6 +130,12 @@
         #endregion
 
         private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.GroupBox InfoGroupBox;
+        private System.Windows.Forms.TextBox AgeTextBox;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button SubmitButton;
     }
 }
 
